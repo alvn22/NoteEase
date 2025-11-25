f454a2e2874db12ba69f8a2cfba82ab8a0c9bc00
JLY2104 <williesfirnanda149@gmail.com>
2025-11-25 17:52:06 +0700

    Tambah Login & Register

---
+++ app/src/main/java/com/example/noteease/RegisterActivity.kt
@@ -0,0 +1,46 @@
+package com.example.noteease // Sesuaikan dengan nama package project Anda
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        val etPhone = findViewById<TextInputEditText>(R.id.etPhone)
+        val etEmail = findViewById<TextInputEditText>(R.id.etEmail)
+        val etPassword = findViewById<TextInputEditText>(R.id.etPassword)
+        val etConfirmPassword = findViewById<TextInputEditText>(R.id.etConfirmPassword)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val phone = etPhone.text.toString().trim()
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+            val confirmPassword = etConfirmPassword.text.toString().trim()
+            if (phone.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Mohon isi semua data!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6) {
+                etPassword.error = "Password minimal 6 karakter"
+                return@setOnClickListener
+            }
+
+            // Cek apakah Password dan Confirm Password sama
+            if (password != confirmPassword) {
+                etConfirmPassword.error = "Password tidak sama!"
+                Toast.makeText(this, "Konfirmasi password salah", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this, "Registrasi Berhasil!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
