c28fa8da1df6e630e98419cb3476bee621b65808
alvn22 <alviandwiky123@gmail.com>
2025-11-27 13:30:48 +0700

    Intent to Login and Register

--- app/src/main/java/com/example/noteease/OnboardingActivity.kt
+++ app/src/main/java/com/example/noteease/OnboardingActivity.kt
@@ -1,10 +1,12 @@
 package com.example.noteease
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,18 @@ class OnboardingActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnRegister = findViewById<MaterialButton>(R.id.btnRegister)
+        val btnLogin = findViewById<MaterialButton>(R.id.btnLogin)
+
+        btnRegister.setOnClickListener {
+            val intent2Register = Intent(this, RegisterActivity::class.java)
+            startActivity(intent2Register)
+        }
+        btnLogin.setOnClickListener {
+            val intent2Login = Intent(this, LoginActivity::class.java)
+            startActivity(intent2Login)
         }
     }
 }
